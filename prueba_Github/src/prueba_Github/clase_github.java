5ca95f69f203ebcd84b232dd6fdb9e6738c8d8e8
maria <maria@mj>
2020-10-17 18:12:02 +0200

    Implemente el bucle que compara dos numeros enteros

--- prueba_Github/src/prueba_Github/clase_github.java
+++ prueba_Github/src/prueba_Github/clase_github.java
@@ -2,7 +2,14 @@ package prueba_Github;
 
 public class clase_github {
 	public static void main(String [] args) {
+		int a=10;
+		int b=11;
 		System.out.println("hola");
 		System.out.println("");
+		if(a>b) {
+			System.out.println("A es mayor que B");
+		}else {
+			System.out.println("A es menor que B");
+		}
 	}
 }
